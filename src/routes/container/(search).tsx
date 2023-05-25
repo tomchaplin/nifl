e7e1b79257c30960d9cab519fe791c7c9cc5941a
Thomas Chaplin <tomrchaplin@gmail.com>
2023-05-25 11:21:19 +0100

    Add sorting arrows to table (non-functional)

--- src/routes/container/(search).tsx
+++ src/routes/container/(search).tsx
@@ -34,24 +34,41 @@ export function routeData() {
 
 export default function ContainerSearch() {
   const [searchParams, setSearchParams] = useSearchParams();
-
   const containers = useRouteData<typeof routeData>();
-  const input_class = "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-2";
 
   const query = searchParams.query == undefined ? "" : searchParams.query;
   const setQuery = (newQuery: string) => {
     setSearchParams({ ...searchParams, query: newQuery })
   };
 
+  const input_class = "appearance-none border w-full rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-2";
   const basic_class = "py-2 px-2";
+  const th_class = basic_class;
+  const th_div_class = "flex justify-start items-center"
 
-  const buildTd = (container: any, contents: any) => (
-    <td>
+  const buildTd = (container: any, contents: any, full_width: boolean) => (
+    <td class={full_width ? "sm:w-3/5" : ""}>
       <A href={`/container/${container.b64_code}`} class={basic_class + " block w-full h-full"}>
         {contents}
       </A>
     </td>
+  )
 
+  // TODO: Make this reactive to query params and add links
+  const buildArrow = (field: string) => {
+    const div_class = "block text-lg cursor-pointer bg-sky-900 px-1 rounded-xl ml-2"
+    return field == 'date_added_iso_string'
+      ? (<div class={div_class}>&darr;</div>)
+      : (<div class={div_class}>&ndash;</div>)
+  }
+
+  const buildTh = (title: string, field: string) => (
+    <th class={th_class}>
+      <div class={th_div_class}>
+        <span>{title}</span>
+        {buildArrow(field)}
+      </div>
+    </th>
   )
 
   return (
@@ -70,18 +87,18 @@ export default function ContainerSearch() {
         <thead
           class="bg-sky-800 text-gray-200">
           <tr>
-            <th class={basic_class}>Contents</th>
-            <th class={basic_class}>Servings</th>
-            <th class={basic_class}>Date Added</th>
+            {buildTh('Contents', 'contents')}
+            {buildTh('Servings', 'servings')}
+            {buildTh('Date Added', 'date_added_iso_string')}
           </tr>
         </thead>
         <tbody>
           <For each={containers()}>
             {(container) =>
               <tr class="hover:bg-sky-100 border-t border-slate-200">
-                {buildTd(container, container.contents)}
-                {buildTd(container, container.servings)}
-                {buildTd(container, container.date_added_iso_string)}
+                {buildTd(container, container.contents, true)}
+                {buildTd(container, container.servings, false)}
+                {buildTd(container, container.date_added_iso_string, false)}
               </tr>
             }
           </For>
